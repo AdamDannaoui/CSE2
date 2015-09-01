3136d10ef4c14b58e70adff66ba297adb4f0ee72
adamdannaoui <htd219@lehigh.edu>
2015-09-01 18:53:24 +0000

    updated hw01

---
+++ hw01.java
@@ -0,0 +1,14 @@
+//Adam Dannaoui hw01 
+public class hw01{ 
+public static void main(String[] args) {
+System.out.println("-----------");
+System.out.println("| WELCOME |");
+System.out.println("-----------");
+System.out.println("  ^  ^  ^  ^  ^  ^  "); 
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+System.out.println("<-H--T--D--2--1--9->");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+System.out.println("  v  v  v  v  v  v  ");
+System.out.println("During the summer i went to the beach.");
+  }
+}
