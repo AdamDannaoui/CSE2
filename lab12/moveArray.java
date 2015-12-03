1c26a43260752151d4a162acbfaf67f8064ba8ab
adamdannaoui <htd219@lehigh.edu>
2015-12-03 00:05:39 +0000

    updated lab 12

---
+++ lab12/moveArray.java
@@ -0,0 +1,52 @@
+//Adam Dannaoui lab 12
+//writing loops and using try and catch statements
+//writing a program that moves the value at a specific index to the end of the array
+import java.util.Scanner; 
+//importing the scanner class
+//class and main method
+public class moveArray{
+    public static void main(String[] args){
+    //initializing the scanner class
+    Scanner myScanner = new Scanner(System.in);
+    //asking for user input
+    System.out.print("Enter the size of the array: ");
+    int size = myScanner.nextInt();
+    int[] array1 = null; 
+    //try and catch statement to check the value entered
+    try{
+        array1 = new int[size];
+    }catch(Exception e){
+        System.out.println("Invalid input"); 
+        
+    }
+    //for loop that runs through the array
+    for(int i=0; i < array1.length; i++){
+        array1[i]=(int)(Math.random()*100); 
+        System.out.print(array1[i] + " "); 
+    }
+    //asking user for input
+    System.out.print("Enter the index of the value to be moved"); 
+    //declaring variables
+    int moveIndex = myScanner.nextInt(); 
+    int temp = 0;
+    //try and catch statement that checks the value entered
+    try{
+        temp = array1[moveIndex];
+    }catch(Exception e){
+        System.out.println("Invalid Input");
+    }
+    //for loops that move that array
+    int i=0;
+    for ( i = moveIndex; i < array1.length - 1; i++){
+        array1[i] = array1[i + 1];
+    }
+    array1[size - 1] = temp; 
+    
+    for (i=0; i < array1.length; i++){
+        //printing the output
+        System.out.print(array1[i] + " ");
+    }
+    
+    }
+    
+    }
